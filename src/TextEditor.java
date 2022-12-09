5d50a240ccdfa7947e2355b84a7d38779baae84e
Suchit Reddi <72243194+SuchitReddi@users.noreply.github.com>
2022-12-09 22:44:06 +0530

    Added cut copy paste etc

--- src/TextEditor.java
+++ src/TextEditor.java
@@ -1,178 +1,304 @@
 
 import java.awt.*;
+import java.awt.datatransfer.*;
 import java.awt.event.*;
 import java.io.*;
 import java.util.*;
+import javax.sound.sampled.Clip;
 import javax.swing.*;
 import javax.swing.filechooser.*;
 
-
-public class TextEditor extends JFrame implements ActionListener{
+public class TextEditor extends JFrame implements ActionListener {
 
     public static void main(String[] args) {
         new TextEditor();
     }
 
+    //<Initialize>
+    String cliptext;
     JFrame frame;
     //Creating the panel
     JPanel panel = new JPanel();
     JTextArea text;
-    JScrollPane scrollPane;
-    JLabel sizelabel;
+    JScrollPane scrollpane;
+    JLabel sizelabel, fontlabel;
     JSpinner spinner;
-    JButton colorbutton;
-    JComboBox fontBox;
-    JMenuBar menuBar;
-    JMenu fileMenu;
-    JMenuItem open;
-    JMenuItem save;
-    JMenuItem exit;
-        TextEditor() {
+    JComboBox<String> fontbox;
+    JMenuBar menubar;
+    JMenu osemenu, ccpmenu;
+    JLabel display = new JLabel("Hi");
+    JMenuItem open, save, exit, cut, copy, paste, delete, find, replace, replaceall;
 
-            //Creating the frame
-            frame = new JFrame("Text Editor");
-            //Setting layout, size, and location of the frame
-            frame.setLayout(new FlowLayout());
-            panel.setLayout(new FlowLayout());
-            frame.setSize(1500, 800);
-            frame.setLocationRelativeTo(panel);
-            //Stop program once we close the window
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//</Initialize>
 
-            //Creating text area for typing
-            text = new JTextArea("Type Here");
-            //Wraps the line when a character reaches the end of line
-            text.setLineWrap(true);
-            //Wraps the whole word the character belongs to
-            text.setWrapStyleWord(true);
-            //Set the default font, type, and size
-            text.setFont(new Font("Times New Roman", Font.PLAIN, 15));
+    //<TextEditor>
+    TextEditor() {
+//<Frame>
+        //Creating the frame
+        frame = new JFrame("Text Editor");
+        //Setting layout, size, and location of the frame
+        frame.setLayout(new FlowLayout());
+        panel.setLayout(new GridLayout());
+        frame.setSize(1500, 800);
+        frame.setLocationRelativeTo(null);
+        //Stop program once we close the window
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//</Frame>
 
-            //Convert the text area scrollable
-            scrollPane = new JScrollPane(text);
-            //setSize doesn't work here, so we have to use setPreferredSize
-            scrollPane.setPreferredSize(new Dimension(1450, 700));
-            //Change the policy to change when the scrollbar should appear
-            scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-            //Horizontal scrollbar is only needed when word wrap is off.
-            //scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
+//<TextArea>
+        //Creating text area for typing
+        text = new JTextArea("Type Here");
+        //Wraps the line when a character reaches the end of line
+        text.setLineWrap(true);
+        //Wraps the whole word the character belongs to
+        text.setWrapStyleWord(true);
+        //Set the default font, type, and size
+        text.setFont(new Font("Times New Roman", Font.PLAIN, 15));
+//</TextArea>
 
-            //Create font size spinner
-            sizelabel = new JLabel("Font Size");
-            spinner = new JSpinner();
-            spinner.setPreferredSize(new Dimension(40, 20));
-            //Setting default value of font size the spinner should show (not the default font size of text)
-            spinner.setValue(15);
-            //We have to add a change listener to actually apply the changes from the spinner
-            spinner.addChangeListener(e -> {
-                //Keep the same font, type and change only the size by listening to the spinner and taking in its value
-                text.setFont(new Font(text.getFont().getFamily(), Font.PLAIN, (int) spinner.getValue()));
-            });
+//<MenuBar>
+        //Creating menu bar
+        menubar = new JMenuBar();
+        //open, save, exit menu
+        osemenu = new JMenu("File");
+        open = new JMenuItem("Open");
+        save = new JMenuItem("Save");
+        exit = new JMenuItem("Exit");
+        //cut, copy, paste menu
+        ccpmenu = new JMenu("Edit");
+        cut = new JMenuItem("Cut");
+        copy = new JMenuItem("Copy");
+        paste = new JMenuItem("Paste");
+        delete = new JMenuItem("Delete");
+        //find, replace, replace-all
+        find = new JMenuItem("Find");
+        replace = new JMenuItem("Replace");
+        replaceall = new JMenuItem("Replace All");
 
-            colorbutton = new JButton("Change Color");
-            colorbutton.addActionListener(this);
+        //Adding action listeners to all functionalities
+        open.addActionListener(this);
+        save.addActionListener(this);
+        exit.addActionListener(this);
+        cut.addActionListener(this);
+        copy.addActionListener(this);
+        paste.addActionListener(this);
+        delete.addActionListener(this);
+        find.addActionListener(this);
+        replace.addActionListener(this);
+        replaceall.addActionListener(this);
 
-            String[] fonts = GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
+        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
 
-            fontBox = new JComboBox(fonts);
-            fontBox.addActionListener(this);
-            fontBox.setSelectedItem("Times New Roman");
+        //Adding all functionalities to menu bar
+        osemenu.add(open);
+        osemenu.add(save);
+        osemenu.add(exit);
+        ccpmenu.add(cut);
+        ccpmenu.add(copy);
+        ccpmenu.add(paste);
+        ccpmenu.add(delete);
+        ccpmenu.add(find);
+        ccpmenu.add(replace);
+        ccpmenu.add(replaceall);
+        menubar.add(display);
+        menubar.add(osemenu);
+        menubar.add(ccpmenu);
+//</MenuBar>
 
-            // ------ menubar ------
+//<ScrollPane>
+        //Convert the text area scrollable
+        scrollpane = new JScrollPane(text);
+        //setSize doesn't work here, so we have to use setPreferredSize
+        scrollpane.setPreferredSize(new Dimension(1450, 700));
+        //Change the policy to change when the scrollbar should appear
+        scrollpane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        //Horizontal scrollbar is only needed when word wrap is off.
+        scrollpane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
+//</ScrollPane>
 
-            menuBar = new JMenuBar();
-            fileMenu = new JMenu("File");
-            open = new JMenuItem("Open");
-            save = new JMenuItem("Save");
-            exit = new JMenuItem("Exit");
+//<FontSize>
+        //Create font size spinner
+        sizelabel = new JLabel("Font Size");
+        spinner = new JSpinner();
+        spinner.setPreferredSize(new Dimension(40, 20));
+        //Setting default value of font size the spinner should show (not the default font size of text)
+        spinner.setValue(15);
+        //We have to add a change listener to actually apply the changes from the spinner
+        spinner.addChangeListener(e -> {
+            //Keep the same font, type and change only the size by listening to the spinner and taking in its value
+            text.setFont(new Font(text.getFont().getFamily(), Font.PLAIN, (int) spinner.getValue()));
+        });
+//</FontSize>
 
-            open.addActionListener(this);
-            save.addActionListener(this);
-            exit.addActionListener(this);
+//<FontChange>
+        //Making a box to change fonts
+        fontlabel = new JLabel("Change Font");
+        //Take all the available font names into an array of strings named fonts
+        String[] fonts = GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
+        //Add the fonts array to the JComboBox
+        fontbox = new JComboBox<>(fonts);
+        //this is used to reference the Action listener in main method
+        fontbox.addActionListener(this);
+        fontbox.setSelectedItem("Times New Roman");
+//</FontChange>
 
-            fileMenu.add(open);
-            fileMenu.add(save);
-            fileMenu.add(exit);
-            menuBar.add(fileMenu);
+//<AddToFrame>
+        frame.setJMenuBar(menubar);
+        frame.add(sizelabel);
+        frame.add(spinner);
+        frame.add(fontlabel);
+        frame.add(fontbox);
+        frame.add(scrollpane);
+        frame.add(panel);
+        panel.add(display);
+        //setVisible should be at last, or else some components won't be visible
+        frame.setVisible(true);
+//</AddToFrame>
+    }
+//</TextEditor>
 
-            // ------ /menubar ------
-
-            frame.setJMenuBar(menuBar);
-            frame.add(sizelabel);
-            frame.add(spinner);
-            frame.add(colorbutton);
-            frame.add(fontBox);
-            frame.add(scrollPane);
-            frame.setVisible(true);
+    //<EventHandler>
+    @Override
+    public void actionPerformed(ActionEvent e) {
+//<FontChange>
+        //If the event is triggered from fontbox
+        if (e.getSource() == fontbox) {
+            //Keep the same type and size, change only the font by listening to the fontbox and taking in its input
+            text.setFont(new Font((String) fontbox.getSelectedItem(), Font.PLAIN, text.getFont().getSize()));
         }
+//</FontChange>
 
-        @Override
-        public void actionPerformed (ActionEvent e){
+//<Open>
+        if (e.getSource() == open) {
+            //Creating filechooser
+            JFileChooser fileChooser = new JFileChooser();
+            //Setting present working directory to current directory(.)
+            fileChooser.setCurrentDirectory(new File("."));
+            //Filtering for text files only
+            FileNameExtensionFilter filter = new FileNameExtensionFilter("Text Files", "txt");
+            fileChooser.setFileFilter(filter);
 
-            if (e.getSource() == colorbutton) {
-                JColorChooser colorChooser = new JColorChooser();
-
-                Color color = colorChooser.showDialog(null, "Choose a color", Color.black);
-
-                text.setForeground(color);
-            }
-
-            if (e.getSource() == fontBox) {
-                text.setFont(new Font((String) fontBox.getSelectedItem(), Font.PLAIN, text.getFont().getSize()));
-            }
-
-            if (e.getSource() == open) {
-                JFileChooser fileChooser = new JFileChooser();
-                fileChooser.setCurrentDirectory(new File("."));
-                FileNameExtensionFilter filter = new FileNameExtensionFilter("Text files", "txt");
-                fileChooser.setFileFilter(filter);
-
-                int response = fileChooser.showOpenDialog(null);
-
-                if (response == JFileChooser.APPROVE_OPTION) {
-                    File file = new File(fileChooser.getSelectedFile().getAbsolutePath());
-                    Scanner fileIn = null;
-
-                    try {
-                        fileIn = new Scanner(file);
-                        if (file.isFile()) {
-                            while (fileIn.hasNextLine()) {
-                                String line = fileIn.nextLine() + "\n";
-                                text.append(line);
-                            }
+            //Choosing whether to open the file or not
+            int response = fileChooser.showOpenDialog(null);
+            //APPROVE_OPTION is yes
+            if (response == JFileChooser.APPROVE_OPTION) {
+                //Get the file path of the selected file
+                File file = new File(fileChooser.getSelectedFile().getAbsolutePath());
+                Scanner input = null;
+                //Exception handling code block in case of unavailable file, etc
+                try {
+                    input = new Scanner(file);
+                    if (file.isFile()) {
+                        //Add the file's lines as long as more lines are scanned using a while loop
+                        while (input.hasNextLine()) {
+                            String line = input.nextLine() + "\n";
+                            text.append(line);
                         }
-                    } catch (FileNotFoundException e1) {
-                        // TODO Auto-generated catch block
-                        e1.printStackTrace();
-                    } finally {
-                        fileIn.close();
                     }
+                } catch (FileNotFoundException e1) {
+                    e1.printStackTrace();
+                } finally {
+                    //Code to display to select some file to open if nothing is selected
+                    if (input == null)
+                        display.setText("Select something!");
+                    assert input != null;
+                    input.close();
                 }
-            }
-            if (e.getSource() == save) {
-                JFileChooser fileChooser = new JFileChooser();
-                fileChooser.setCurrentDirectory(new File("."));
-
-                int response = fileChooser.showSaveDialog(null);
-
-                if (response == JFileChooser.APPROVE_OPTION) {
-                    File file;
-                    PrintWriter fileOut = null;
-
-                    file = new File(fileChooser.getSelectedFile().getAbsolutePath());
-                    try {
-                        fileOut = new PrintWriter(file);
-                        fileOut.println(text.getText());
-                    } catch (FileNotFoundException e1) {
-                        // TODO Auto-generated catch block
-                        e1.printStackTrace();
-                    } finally {
-                        fileOut.close();
-                    }
-                }
-            }
-            if (e.getSource() == exit) {
-                System.exit(0);
             }
         }
+//</Open>
+
+//<Save>
+        //If the event is triggered from save
+        if (e.getSource() == save) {
+            //Create filechooser
+            JFileChooser fileChooser = new JFileChooser();
+            //Set the present working directory to current directory(.)
+            fileChooser.setCurrentDirectory(new File("."));
+
+            //Selecting whether to save the file or not
+            int response = fileChooser.showSaveDialog(null);
+            //APPROVE_OPTION is yes
+            if (response == JFileChooser.APPROVE_OPTION) {
+                //get the absolute path of the selected file
+                File file = new File(fileChooser.getSelectedFile().getAbsolutePath());
+                //initializing the variable output
+                PrintWriter output = null;
+                //Create exception handling block
+                try {
+                    output = new PrintWriter(file);
+                    output.println(text.getText());
+                } catch (FileNotFoundException e1) {
+                    e1.printStackTrace();
+                } finally {
+                    //Code to make sure that file name is typed
+                    if (output == null)
+                        display.setText("Type something!");
+                    assert output != null;
+                    output.close();
+                }
+            }
+        }
+//</Save>
+
+//<Exit>
+        //If the event is triggered from exit
+        if (e.getSource() == exit) {
+            //Exits the program without any errors
+            System.exit(0);
+        }
+//</Exit>
+
+//<Cut>
+        if (e.getSource() == cut) {
+            //Initialize and get system's keyboard
+            Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+            //get text from text area
+            StringSelection data = new StringSelection(text.getText());
+            //copy the contents to clipboard
+            clipboard.setContents(data, data);
+            //replace the text area with null
+            text.setText("");
+        }
+//</Cut>
+
+//<Copy>
+        if (e.getSource() == copy) {
+            //Initialize and get system's keyboard
+            Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+            //get text from text area
+            StringSelection data = new StringSelection(text.getText());
+            //copy the contents to clipboard
+            clipboard.setContents(data, data);
+        }
+//</Copy>
+
+//<Paste>
+        if (e.getSource() == paste) {
+            //Initialize clipboard
+            Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+            //Transfer clipboard data to clipdata
+            Transferable clipdata = clipboard.getContents(this);
+            //Exception handling code block
+            try{
+                //Transfer the data in clipdata to cliptext
+                cliptext = (String) (clipdata.getTransferData(DataFlavor.stringFlavor));
+            }
+            catch (Exception e1){
+                e1.printStackTrace();
+            }
+            //Change the original text in text area to cliptext
+            text.setText(cliptext);
+        }
+//</Paste>
+
+//<Delete>
+        if (e.getSource() == delete) {
+            //replace the text area with null
+            text.setText("");
+        }
+//</Delete>
+
+//</EventHandler>
+    }
 }
